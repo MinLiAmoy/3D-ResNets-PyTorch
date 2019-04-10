5338979c96fb0f57a65a134b185ef4397b1cc835
MinLiAmoy <minlixm@sina.com>
2019-04-10 22:34:30 +0800

    fixed bugs, now Mixture/test.py can work

--- Mixture/test.py
+++ Mixture/test.py
@@ -18,38 +18,39 @@ if __name__ == '__main__':
 	parser.add_argument(
 		'--clean_path', default='UCF_inputs.npy', type=str, help='the path for clean examples')
 	parser.add_argument(
+		'--label_path', default='UCF_labels.npy', type=str, help='the path for clean eamamples labels')
+	parser.add_argument(
 		'--sample_rate', default=4, type=int, help='the sample_rate for adv examples, eg. 4-sample once for every 4 frames')
 
+	args = parser.parse_args()
 	# load model
 	raw_model_location = 'resnet-18-kinetics-ucf101_split1.pth'
 	checkpoint = torch.load(raw_model_location)
 	raw_model = resnet.resnet18(num_classes=101, shortcut_type='A', sample_size=112, sample_duration=16)
 	raw_model = raw_model.cuda()
+	raw_model = torch.nn.DataParallel(raw_model)
 	raw_model.load_state_dict(checkpoint['state_dict'])
 
 
 
 	# subsample the adversarial examples and replace the corresponding ones in clean data
-	adv_stack = np.load(adv_path)
-	clean_stack = np.load(clean_path)
+	adv_stack = np.load(args.adv_path)
+	clean_stack = np.load(args.clean_path)
+	labels_stack = np.load(args.label_path)
 	mix_stack = np.copy(clean_stack)
 
-	mix_stack[:,:,:sample_rate:,:,:] = adv_stack[:,:,:sample_rate:,:,:]
+	mix_stack[:,:,::args.sample_rate,:,:] = adv_stack[:,:,::args.sample_rate,:,:]
 
 	# Inference
-	mix_labels = predict_batch(model=raw_model, samples=mix_stack, batch=10, device='cuda')
-    mix_labels = np.array(mix_labels)
-    mix_labels = torch.max(torch.from_numpy(mix_labels), 1)[1]
-    mix_labels = mix_labels.cpu().numpy()
+	mix_labels = utils.predict_batch(model=raw_model, samples=mix_stack, batch=10, device='cuda')
+	mix_labels = np.array(mix_labels)
+	mix_labels = torch.max(torch.from_numpy(mix_labels), 1)[1]
+	mix_labels = mix_labels.cpu().numpy()
 
-    # np.save('{}{}_AdvExamples_{}.npy'.format(self.adv_examples_dir, self.attack_name, self.epsilon/255.), adv_samples)
-    # np.save('{}{}_AdvLabels_{}.npy'.format(self.adv_examples_dir, self.attack_name, self.epsilon/255.), adv_labels)
-    # np.save('{}{}_TrueLabels_{}.npy'.format(self.adv_examples_dir, self.attack_name, self.epsilon/255.), self.labels_samples)
-
-    mis = 0
-    for i in range(len(mix_stack)):
-        if mix_labels[i].argmax(axis=0) != adv_labels[i]:
-            mis = mis + 1
-    print('\nFor **{}** on **{}**: misclassification ratio is {}/{}={:.1f}%\n'.format(self.attack_name, self.dataset, mis, len(adv_samples),
-                                                                                          mis / len(adv_labels) * 100))
+	mis = 0
+	for i in range(len(mix_stack)):
+		if labels_stack[i].argmax(axis=0) != mix_labels[i]:
+			mis = mis + 1
+	print('\nmisclassification ratio is {}/{}={:.1f}%\n'.format(mis, len(mix_stack),
+                                                                                          mis / len(mix_stack) * 100))
 
